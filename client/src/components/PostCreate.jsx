0c81e6f1cbed257d6a2a5e18e3d579726df87258
Rob Ranf <rob@emiyaconsulting.com>
2021-07-03 21:06:27 -0700

    starting client

---
+++ client/src/components/PostCreate.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const PostCreate = () => {
+	const [title, setTitle] = useState("");
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+
+		await axios.post("http://localhost:4000/posts", { title });
+
+		console.log(title);
+
+		setTitle("");
+	};
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<div className="form-group mb-4">
+					<label className="mb-2">Title</label>
+					<input
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						className="form-control"
+					/>
+				</div>
+				<button className="btn btn-primary">Submit</button>
+			</form>
+		</div>
+	);
+};
+
+export default PostCreate;
