69a6663b55c57a2128603195ef0ba851f11c9454
Rob Ranf <rob@emiyaconsulting.com>
2022-04-20 18:07:50 -0700

    adding clusterip services on posts and event-bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,17 +13,17 @@ app.post("/events", async (req, res) => {
 	events.push(event);
 
 	await axios
-		.post("http://localhost:4000/events", event)
+		.post("http://posts-cluster-srv:4000/events", event)
 		.catch((err) => console.log(err.message));
-	await axios
-		.post("http://localhost:4001/events", event)
-		.catch((err) => console.log(err.message));
-	await axios
-		.post("http://localhost:4002/events", event)
-		.catch((err) => console.log(err.message));
-	await axios
-		.post("http://localhost:4003/events", event)
-		.catch((err) => console.log(err.message));
+	// await axios
+	// 	.post("http://localhost:4001/events", event)
+	// 	.catch((err) => console.log(err.message));
+	// await axios
+	// 	.post("http://localhost:4002/events", event)
+	// 	.catch((err) => console.log(err.message));
+	// await axios
+	// 	.post("http://localhost:4003/events", event)
+	// 	.catch((err) => console.log(err.message));
 	res.status("200").json({ success: true });
 });
 
